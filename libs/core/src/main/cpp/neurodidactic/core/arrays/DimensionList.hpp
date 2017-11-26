ac5e66420c06a9395d3638d872be92b75a5761c3
Tom Ault <tomault@cs.cmu.edu>
2017-11-26 07:05:26 -0500

    Modified DimensionList to inherit from ImmutableList

--- libs/core/src/main/cpp/neurodidactic/core/arrays/DimensionList.hpp
+++ libs/core/src/main/cpp/neurodidactic/core/arrays/DimensionList.hpp
@@ -1,8 +1,8 @@
 #ifndef __NEURODIDACTIC__CORE__ARRAYS__DIMENSIONLIST_HPP__
 #define __NEURODIDACTIC__CORE__ARRAYS__DIMENSIONLIST_HPP__
 
-#include <allocator>
-#include <memory>
+#include <pistis/util/ImmutableList.hpp>
+#include <type_traits>
 #include <stdint.h>
 
 namespace neurodidactic {
@@ -10,69 +10,52 @@ namespace neurodidactic {
     namespace arrays {
 
       template <typename Allocator = std::allocator<uint32_t> >
-      class DimensionList : Allocator {
+      class DimensionList : public ImmutabeList<uint32_t, Allocator> {
       public:
-	typedef Allocator AllocatorType;
-
-      public:
-	template <typename Iterator>
+	template <typename Iterator,
+		  typename Enabler =
+		      typename std::enable_if<
+		          !std::is_integral<Iterator>::value, int
+		      >::type
+		  >
 	DimensionList(uint32_t numDimensions, Iterator dimensions,
-		      const Allocator& allocator):
-	    Allocator(allocator), numDimensions_(numDimensions),
-	    dimensions_(allocator.allocate(numDimensions)) {
-	  std::copy_n(dimensions, numDimensions, dimensions_.get());
+		      const Allocator& allocator = Allocator(),
+		      Enabler = 0):
+	    ImmutableList<uint32_t, Allocator>(numDimensions, dimensions,
+					       allocator) {
 	}
 
+	template <typename Iterator,
+		  typename Enabler =
+		      typename std::enable_if<
+		          !std::is_integral<Iterator>::value, int
+		      >::type
+		  >
+	DimensionList(Iterator startOfDimensions,
+		      Iterator endOfDimensions,
+		      const Allocator& allocator = Allocator(),
+		      Enabler = 0):
+	    ImmutableList<uint32_t, Allocator>(startOfDimensions,
+					       endOfDimensions
+					       allocator) {
+	}
+	
 	DimensionList(const std::initializer_list<uint32_t>& dimensions,
 		      const Allocator& allocator):
-	    Allocator(allocator), numDimensions_(dimensions.size()),
-	    dimensions_(allocator.allocate(numDimensions)) {
-	  std::copy(dimensions.begin(), dimensions.end(), dimensions_.get());
+	    ImmutableList<uint32_t, Allocator>(dimensions, allocator) {
 	}
 	
-	DimensionList(const DimensionList<Allocator>& other):
-	    Allocator(other.allocator()), numDimensions_(other.size()),
-	    dimensions_(other.dimensions_) {
-	  // Intentionally left blank
-	}
+	DimensionList(const DimensionList<Allocator>& other) = default;
 
-	const Allocator& allocator() const {
-	  return static_cast<const Allocator&>(*this);
-	}
-	Allocator& allocator() { return static_cast<Allocator&>(*this); }
-	
-	uint32_t size() const { return numDimensions_; }
-	const uint32_t* begin() const { return dimensions_.get(); }
-	const uint32_t* end() const { return dimensions_.get() + size(); }
 	uint64_t numElements() const {
-	  if (!numElements) {
-	    return 0;
-	  } else {
-	    uint64_t n = dimensions_[0];
-	    const uint64_t* const endOfDimensions = end();
-	    for (const uint64_t* p = begin() + 1; p != endOfDimensions; ++p) {
-	      n *= *p;
-	    }
-	    return n;
-	  }
+	  static const auto PRODUCT = [](uint32_t product, uint32_t value) {
+	    return product * value;
+	  };
+	  
+	  return empty() ? 0 : reduce(PRODUCT, 1);
 	}
-	
-	DimensionList<Allocator>& operator=(
-	    const DimensionList<Allocator>& other
-	) {
-	  if (this != &other) {
-	    allocator() = other.allocator();
-	    numDimensions_ = other.numDimensions_;
-	    dimensions_ = other.dimensions_;
-	  }
-	  return *this;
-	}
-	
-	uint32_t operator[](uint32_t n) const { return dimensions_[n]; }
 	
       private:
-	uint32_t numDimensions_;
-	std::shared_ptr<uint32_t[]> dimensions_;
       };
 
     }
--- libs/core/src/test/cpp/neurodidactic/core/arrays/DimensionListTests.cpp
+++ libs/core/src/test/cpp/neurodidactic/core/arrays/DimensionListTests.cpp
@@ -11,12 +11,7 @@ TEST(DimensionListTests, CreateFromData) {
   const pt::Allocator<uint32_t> allocator("TEST_1");
   DimensionList< pt::Allocator<uint32_t> > l(NUM_DATA, DATA, allocator);
 
-  EXPECT_EQ(NUM_DATA, l.size());
-  EXPECT_EQ(NUM_DATA, l.end() - l.begin());
   EXPECT_EQ("TEST_1", l.allocator().name());
-  EXPECT_EQ("TEST_1",
-	    ((const DimensionList< pt::Allocator<uint32_t> >&)l).allocator()
-	                                                        .name()).
 
   std::vector<uint32_t> truth(DATA, DATA + NUM_DATA);
   std::vector<uint32_t> dimensions(l.begin(), l.end());
@@ -26,78 +21,12 @@ TEST(DimensionListTests, CreateFromData) {
 TEST(DimensionListTests, CreateFromInitializerList) {
   const pt::Allocator<uint32_t> allocator("TEST_1");
   DimensionList< pt::Allocator<uint32_t> > l{{ 5, 3, 2, 7 }, allocator};
-  const DimensionList< pt::Allocator<uint32_t> >& cl = l;
 
-  EXPECT_EQ(4, l.size());
-  EXPECT_EQ(4, l.end() - l.begin());
   EXPECT_EQ("TEST_1", l.allocator().name());
-  EXPECT_EQ("TEST_1", cl.allocator().name());
 
   std::vector<uint32_t> truth{ 5, 3, 2, 7 };
   std::vector<uint32_t> dimensions(l.begin(), l.end());
   EXPECT_EQ(truth, dimensions);
-}
-
-TEST(DimensionListTests, Copy) {
-  const std::vector<uint32_t> TRUTH{ 5, 3, 2, 7 };
-  const pt::Allocator<uint32_t> allocator("TEST_1");
-  DimensionList< pt::Allocator<uint32_t> > l(TRUTH.size(), TRUTH.begin(),
-					     allocator);
-
-  EXPECT_EQ(TRUTH.size(), l.size());
-  EXPECT_EQ("TEST_1", l.allocator().name());
-  std::vector<uint32_t> dimensions(l.begin(), l.end());
-  EXPECT_EQ(TRUTH, dimensions);
-
-  {
-    DimensionList< pt::Alllocator<uint32_t> > copy(l);
-
-    EXPECT_EQ(TRUTH.size(), copy.size());
-    EXPECT_EQ("TRUTH_1", copy.allocator().name());
-
-    std::vector<uint32_t> copyDimensions(copy.begin(), copy.end());
-    EXPECT_EQ(TRUTH, copyDimensions);
-  }
-
-  EXPECT_EQ(TRUTH.size(), l.size());
-  EXPECT_EQ("TEST_1", l.allocator().name());
-  dimensions = std::vector<uint32_t>(l.begin(), l.end());
-  EXPECT_EQ(TRUTH, dimensions);
-}
-
-TEST(DimensionListTests, CopyAssignment) {
-  const std::vector<uint32_t> TRUTH{ 5, 3, 2, 7 };
-  const pt::Allocator<uint32_t> allocator("TEST_1");
-  DimensionList< pt::Allocator<uint32_t> > l(TRUTH.size(), TRUTH.begin(),
-					     allocator);
-
-  EXPECT_EQ(TRUTH.size(), l.size());
-  EXPECT_EQ("TEST_1", l.allocator().name());
-  std::vector<uint32_t> dimensions(l.begin(), l.end());
-  EXPECT_EQ(TRUTH, dimensions);
-
-  {
-    const std::vector<uint32_t> COPY_INITIAL{ 3, 2, 4 };
-    const pt::Allocator<uint32_t> copyAllocator("TEST_2");
-    DimensionList< pt::Allocator<uint32_t> > copy(COPY_INITIAL.size(),
-						  COPY_INITIAL.begin(),
-						  copyAllocator);
-    std::vector<uint32_t> copyDimensions(copy.begin(), copy.end());
-
-    EXPECT_EQ("TEST_2", copy.allocator().name());
-    EXPECT_EQ(COPY_INITIAL, copyDimensions);
-
-    copy = l;
-    copyDimensions = std::vector<uint32_t>(copy.begin(), copy.end());
-
-    EXPECT_EQ("TEST_1", copy.allocator().name());
-    EXPECT_EQ(TRUTH.size(), copy.size());
-    EXPECT_EQ(TRUTH, copyDimensions);
-  }
-
-  dimensions = std::vector<uint32_t>(l.begin(), l.end());
-  EXPECT_EQ("TEST_1", l.allocator().name());
-  EXPECT_EQ(TRUTH, dimensions);  
 }
 
 TEST(DimensionListTests, NumElements) {
@@ -106,15 +35,3 @@ TEST(DimensionListTests, NumElements) {
 
   EXPECT_EQ(280, l.numElements());
 }
-
-TEST(DimensionListTests, RandomAccess) {
-  pt::Allocator<uint32_t> allocator("TEST_1");
-  DimensionList< pt::Allocator<uint32_t> > l{ { 2, 7, 4, 5 }, allocator };
-
-  ASSERT_EQ(4, l.size());
-  EXPECT_EQ(2, l[0]);
-  EXPECT_EQ(7, l[1]);
-  EXPECT_EQ(4, l[2]);
-  EXPECT_EQ(5, l[3]);
-}
-
